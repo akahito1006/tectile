fcdf9380ab77b61e98e6110720e726746a62385a
akahito1006 <nekodora5k@gmail.com>
2021-04-06 09:04:31 +0000

    built seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,12 +22,27 @@ Admin.create!(
 3.times do |n|
   Article.create!(
     user_id: n+1,
-    title: "#{n + 1}件目の投稿記事",
+    title: "#{n + 1}件目の投稿記事のタイトル",
     content: "Lorem ipsum dolor sit amet,\r\n
     consectetur adipiscing elit, \r\n
     sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. \r\n
     Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. \r\n
     Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. \r\n
     Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-    )
+  )
+end
+
+2.times do |n|
+  Like.create!(
+    user_id: n+2,
+    article_id: n+1
+  )
+end
+
+2.times do |n|
+  Comment.create!(
+    user_id: n+2,
+    article_id: n+1,
+    body: "私も同感です。Lorem ipsum dolor sit amet"
+  )
 end
