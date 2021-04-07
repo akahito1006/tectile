891a9efbe65695f481a7840607f02bbf52d4e8d2
akahito1006 <nekodora5k@gmail.com>
2021-04-07 08:29:08 +0000

    routings and namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,34 @@
 Rails.application.routes.draw do
-  devise_for :admin
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  devise_for :admin, controllers: {
+    sessions:       'admin/sessions',
+    passwords:      'admin/passwords',
+    registrations:  'admin/registrations'
+  }
+  devise_for :users, controllers: {
+    sessions:       'public/sessions',
+    passwords:      'public/passwords',
+    registrations:  'public/registrations'
+  }
+
+  scope module: :public do
+    root 'homes#top'
+    get '/about' => 'homes#about'
+    resources :articles,  only: [:index, :show, :new, :create, :edit, :update, :destroy]
+    resources :users,     only: [:show, :edit, :update, :destroy]
+    resource  :likes,     only: [:create, :destroy]
+    resources :comments,  only: [:create, :destroy]
+    get '/users/unsubscribe' => 'users#unsubscribe'
+    get '/search' => 'searches#search'
+  end
+
+  namespace :admin do
+    resources :articles,  only: [:index, :show, :edit, :update, :destroy]
+    resources :users,     only: [:index, :show, :edit, :update, :destroy]
+    resource  :likes,     only: [:create, :destroy]
+    resources :comments,  only: [:create, :destroy]
+    get '/users/unsubscribe' => 'users#unsubscribe'
+    get '/search' => 'searches#search'
+  end
+  
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,41 +8,42 @@
 
 3.times do |n|
   User.create!(
-    name: "testuser#{n + 1}",
-    email: "testuser#{n + 1}@mail.com",
+    name:     "testuser#{n + 1}",
+    email:    "testuser#{n + 1}@mail.com",
     password: "testpw"
   )
 end
 
 Admin.create!(
-  email: "admin@admin.com",
-  password: "administrator"
+  email:      "admin@admin.com",
+  password:   "administrator"
 )
 
 3.times do |n|
   Article.create!(
-    user_id: n+1,
-    title: "#{n + 1}件目の投稿記事のタイトル",
-    content: "Lorem ipsum dolor sit amet,\r\n
-    consectetur adipiscing elit, \r\n
-    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. \r\n
-    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. \r\n
-    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. \r\n
-    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    user_id:  n+1,
+    title:    "#{n + 1}件目の投稿記事のタイトル",
+    content:
+      "Lorem ipsum dolor sit amet,\r\n
+      consectetur adipiscing elit, \r\n
+      sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. \r\n
+      Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. \r\n
+      Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. \r\n
+      Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
   )
 end
 
 2.times do |n|
   Like.create!(
-    user_id: n+2,
-    article_id: n+1
+    user_id:      n+2,
+    article_id:   n+1
   )
 end
 
 2.times do |n|
   Comment.create!(
-    user_id: n+2,
-    article_id: n+1,
+    user_id:      n+2,
+    article_id:   n+1,
     body: "私も同感です。Lorem ipsum dolor sit amet"
   )
 end
