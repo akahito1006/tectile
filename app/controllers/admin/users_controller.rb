adc5d0a5d65555a533377a4bd25b34b882cf5e6e
akahito1006 <nekodora5k@gmail.com>
2021-04-13 10:55:08 +0000

    管理者側会員機能、画面作成

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,34 @@
+class Admin::UsersController < ApplicationController
+  
+  def index
+    @users = User.all
+  end
+  
+  def show
+    @user = User.where(id: params[:id]).eager_load(:articles, :likes)
+    @articles = Article.where(user_id: params[:id]).eager_load(:images)
+    # a user has_many articles, an article has_many images. *a user has no images*
+    # ---------------after edit
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to admin_users_path
+  end
+  
+  def destroy
+    @user = User.find(params[:id]).destroy
+    redirect_to root_path
+  end
+  
+private
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
+  
+end
