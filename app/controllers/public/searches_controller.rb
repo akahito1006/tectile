1b2e3a8a9c9890c4fe67a1424803c0b9871fe537
akahito1006 <nekodora5k@gmail.com>
2021-04-18 09:21:27 +0000

    検索機能

---
+++ app/controllers/public/searches_controller.rb
@@ -0,0 +1,6 @@
+class Public::SearchesController < ApplicationController
+  def search
+    @word_for_search = params[:word_for_search]
+    @articles = Article.where("title LIKE ?", "%#{@word_for_search}%").eager_load(:user, :likes)
+  end
+end
