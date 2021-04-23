d191ed2fac407c1ba59a868d5726ae975a6d1c3a
akahito1006 <nekodora5k@gmail.com>
2021-04-23 07:02:08 +0000

    whenever

--- lib/batch/data_reset.rb
+++ lib/batch/data_reset.rb
@@ -1,5 +1,5 @@
 class Batch::DataReset
   def self.data_reset
-    exec("rails db:reset")
+    exec("RAILS_ENV=production DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:reset")
   end
 end
