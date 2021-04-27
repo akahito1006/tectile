96eb51a0a2adb45bb5758a4bb31d734ed1c3b30d
akahito1006 <nekodora5k@gmail.com>
2021-04-27 07:38:41 +0000

    seed

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,6 +1,6 @@
 class Public::HomesController < ApplicationController
   def top
-    @articles = Article.limit(3).eager_load(:user, :likes, :article_images)
+    @articles = Article.order(created_at: "DESC").limit(3).eager_load(:user, :likes, :article_images)
   end
 
   def about
