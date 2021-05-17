041a8477d3cb5da3d0f1f63683628efc7b7b15ed
akahito1006 <nekodora5k@gmail.com>
2021-05-17 07:36:41 +0000

    append true

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,7 +3,7 @@ class Article < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :article_images, dependent: :destroy
-  accepts_attachments_for :article_images, attachment: :image
+  accepts_attachments_for :article_images, attachment: :image, append: true
 
   validates :user_id, presence: true
   validates :title  , presence: true
