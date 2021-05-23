44c6aacab464795f6dc4f25a5857929533be65d6
akahito1006 <nekodora5k@gmail.com>
2021-05-23 23:19:50 +0000

    refactoring

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -2,11 +2,7 @@ class Public::CommentsController < ApplicationController
   before_action :authenticate_user!, only: [:create, :destroy]
 
   def create
-    begin
-      @article = Article.find(params[:article_id])
-    rescue
-      render "index"
-    end
+    set_article(params[:article_id])
     @comment = Comment.new(comment_params)
     @comment.user_id = current_user.id
     @comment.article_id = @article.id
@@ -14,11 +10,7 @@ class Public::CommentsController < ApplicationController
   end
 
   def destroy
-    begin
-      @article = Article.find(params[:article_id])
-    rescue
-      render "index"
-    end
+    set_article(params[:article_id])
     comment = current_user.comments.find_by(article_id: @article.id)
     comment.destroy
   end
--- app/controllers/public/likes_controller.rb
+++ app/controllers/public/likes_controller.rb
@@ -1,22 +1,15 @@
 class Public::LikesController < ApplicationController
-  before_action :authenticate_user!, :set_article,  only: [:create, :destroy]
+  before_action :authenticate_user!,  only: [:create, :destroy]
 
   def create
+    set_article(params[:article_id])
     like = current_user.likes.new(article_id: @article.id)
     like.save
   end
 
   def destroy
+    set_article(params[:article_id])
     like = current_user.likes.find_by(article_id: @article.id)
     like.destroy
-  end
-  
-  private
-  def set_article
-    begin
-      @article = Article.find(params[:article_id])
-    rescue
-      redirect_to "/", notice: "エラーが発生しました"
-    end
   end
 end
